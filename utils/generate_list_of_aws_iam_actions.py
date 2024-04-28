fe851333c8d435217c572aff8c468636248338b2
michael-kirchner-at <michael.kirchner@welldone.cloud>
2024-04-28 07:11:17 +0200

    Updated script to dump IAM actions

--- utils/generate_list_of_aws_iam_actions.py
+++ utils/generate_list_of_aws_iam_actions.py
@@ -17,15 +17,15 @@ if __name__ == "__main__":
     policy_editor_js_response = policy_editor_js_response.removeprefix(AWS_POLICY_EDITOR_JS_CONFIG_PREFIX)
 
     policy_data = json.loads(policy_editor_js_response)
-    all_api_actions = []
+    all_iam_actions = []
     for service in policy_data["serviceMap"]:
         for action in policy_data["serviceMap"][service]["Actions"]:
-            all_api_actions.append("{}:{}".format(policy_data["serviceMap"][service]["StringPrefix"], action))
+            all_iam_actions.append("{}:{}".format(policy_data["serviceMap"][service]["StringPrefix"], action))
 
     timestamp = datetime.datetime.now(datetime.timezone.utc).strftime(TIMESTAMP_FORMAT)
-    result_file = "aws_api_actions_{}.txt".format(timestamp)
+    result_file = "aws_iam_actions_{}.txt".format(timestamp)
     with open(result_file, "w") as out_file:
-        for line in sorted(all_api_actions):
+        for line in sorted(all_iam_actions):
             out_file.write("{}\n".format(line))
 
     print("Results written to {}".format(result_file))
